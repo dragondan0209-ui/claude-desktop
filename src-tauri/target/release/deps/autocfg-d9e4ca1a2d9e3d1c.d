3628a5caf3c3feb9b6a4f0f7a332398ef521ac54
zhaodan01 <zhaodan01@jiuqi.com.cn>
2026-04-02 15:12:33 +0800

    第二次

---
+++ src-tauri/target/release/deps/autocfg-d9e4ca1a2d9e3d1c.d
@@ -0,0 +1,10 @@
+D:\workspace\claude-desktop\src-tauri\target\release\deps\autocfg-d9e4ca1a2d9e3d1c.d: C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\lib.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\error.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\rustc.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\version.rs
+
+D:\workspace\claude-desktop\src-tauri\target\release\deps\libautocfg-d9e4ca1a2d9e3d1c.rlib: C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\lib.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\error.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\rustc.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\version.rs
+
+D:\workspace\claude-desktop\src-tauri\target\release\deps\libautocfg-d9e4ca1a2d9e3d1c.rmeta: C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\lib.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\error.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\rustc.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\version.rs
+
+C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\lib.rs:
+C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\error.rs:
+C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\rustc.rs:
+C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\version.rs:
---
+++ src-tauri/target/release/deps/utf8_iter-32ebd908a03b4ee9.d
@@ -0,0 +1,9 @@
+D:\workspace\claude-desktop\src-tauri\target\release\deps\utf8_iter-32ebd908a03b4ee9.d: C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\utf8_iter-1.0.4\src\lib.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\utf8_iter-1.0.4\src\indices.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\utf8_iter-1.0.4\src\report.rs
+
+D:\workspace\claude-desktop\src-tauri\target\release\deps\libutf8_iter-32ebd908a03b4ee9.rlib: C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\utf8_iter-1.0.4\src\lib.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\utf8_iter-1.0.4\src\indices.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\utf8_iter-1.0.4\src\report.rs
+
+D:\workspace\claude-desktop\src-tauri\target\release\deps\libutf8_iter-32ebd908a03b4ee9.rmeta: C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\utf8_iter-1.0.4\src\lib.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\utf8_iter-1.0.4\src\indices.rs C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\utf8_iter-1.0.4\src\report.rs
+
+C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\utf8_iter-1.0.4\src\lib.rs:
+C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\utf8_iter-1.0.4\src\indices.rs:
+C:\Users\dan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\utf8_iter-1.0.4\src\report.rs:
